060323cb2df9e4dba0d46d2e62526bd8a8eec12f
CuongVT <kevinvuong.mim@gmail.com>
2025-12-08 13:54:01 +0700

    chore: remove unused Prisma configuration file and update TypeScript build settings

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { env, defineConfig } from 'prisma/config';
-
-export default defineConfig({
-  engine: 'classic',
-  schema: 'prisma/schema.prisma',
-  datasource: { url: env('DATABASE_URL') },
-  migrations: { path: 'prisma/migrations' },
-});
